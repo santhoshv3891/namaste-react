763a7aecfcbaadc75a64f93200cc7e15f95bf364
Santhosh.V <santhosh.v4@cognizant.com>
2023-09-08 12:03:20 +0530

    Episode 3 work

--- js/App.js
+++ js/App.js
@@ -1,4 +1,15 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello World!!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => <h2>This is the title</h2>;
+
+const Heading = () => (
+  <div class="container">
+    <h1>This is the main heading</h1>
+    <Title />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<Heading />);
